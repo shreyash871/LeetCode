763e86f6ff1a99f178d15af0892a71caed3db8bc
shreyashbkone-pixel <shreyashbkone4@gmail.com>
2026-08-08 23:42:40 +0530

    Time: 0 ms (100%), Space: 11.2 MB (83.58%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+bool check(vector<int>& nums)
+{
+    int n = nums.size();
+    int count = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (nums[i] > nums[i + 1])
+        {
+            count++;
+        }
+    }
+
+    // Now check the circular pair:
+    // nums[n-1] and nums[0]
+
+    if (nums[n - 1] > nums[0])
+    {
+        count++;
+    }
+
+    return count <= 1;
+}
+};
